6b7db96a0fc43c832f492aec3aa2d8bf597a1bf4
Arhan <arhandonerkaya20@gmail.com>
2025-05-17 19:20:16 +0300

    CORS middleware eklendi

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from googletrans import Translator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 translator = Translator()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # geçici olarak herkese açık (güvenli değilse sonra sınırlarız)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CeviriIstegi(BaseModel):
     q: str
